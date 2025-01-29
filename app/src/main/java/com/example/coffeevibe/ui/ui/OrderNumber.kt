e696eaf72c7ad046489dd873463f1f95414619d8
Milkingooo <maks.trushin.06@bk.ru>
2025-01-29 11:50:47 +0300

    dds

---
+++ app/src/main/java/com/example/coffeevibe/ui/ui/OrderNumber.kt
@@ -0,0 +1,66 @@
+package com.example.coffeevibe.ui.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.coffeevibe.R
+import com.example.coffeevibe.ui.theme.CoffeeVibeTheme
+
+@Composable
+fun OrderNumber() {
+    CoffeeVibeTheme(content = {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(116.dp)
+                .background(colorScheme.surface, RoundedCornerShape(16.dp)),
+            colors = CardDefaults.cardColors(containerColor = colorScheme.surface),
+            shape = RoundedCornerShape(22.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Номер заказа: 52",
+                    color = colorScheme.onBackground,
+                    fontFamily = FontFamily(Font(R.font.roboto_condensed_bold)),
+                    fontSize = 26.sp
+                )
+                Text(
+                    text = "150 руб.",
+                    color = colorScheme.onBackground,
+                    fontFamily = FontFamily(Font(R.font.roboto_condensed_bold)),
+                    fontSize = 26.sp
+                )
+
+            }
+        }
+    })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OrderNumPreview() {
+    OrderNumber()
+}
